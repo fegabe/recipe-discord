a3896dd8390a9b5e756ab209918cb955c412c16e
Fernando Garcia Bernal <fegabe@gmail.com>
2020-06-12 12:46:34 +0200

    Fix to show discord badge counter

--- webview.js
+++ webview.js
@@ -1,10 +1,15 @@
-import path from 'path';
+const path = require('path');
 
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
     // get unread messages
-    const direct = document.querySelectorAll('[class^="guildsWrapper"] [class*="badge"]').length;
-    const indirect = document.querySelectorAll('[class^="guildsWrapper"] [class^="guild-"]+[class*="unread-"]').length;
+    const serverbar = document.querySelector('[class^=unreadMentionsIndicatorTop] + div');
+    if (!serverbar) return;
+    const direct = serverbar.querySelectorAll('[class*="lowerBadge"]').length;
+    let channelsWithDots = document.querySelectorAll('[class*="pill-"] span[style*="height: 8px"]').length;
+    let mutedChannelOnFocusWithUnreads = document.querySelectorAll('[class*="containerDefault-"] div[class*="modeMuted-"][aria-label*="unread"]').length;
+    let channelOnFocusWithUnreads = document.querySelectorAll('[class*="containerDefault-"] div[aria-label*="unread"]').length;
+    let indirect = channelsWithDots + (channelOnFocusWithUnreads - mutedChannelOnFocusWithUnreads);
 
     // set Franz badge
     Franz.setBadge(direct, indirect);
